b3b855dfc385e6ad4b16f5ab624046d21dd8fad7
NihadMouden <nihadmouden@gmail.com>
2023-03-15 01:42:36 +0100

    Update1

--- Readme.md
+++ Readme.md
@@ -27,13 +27,13 @@ En esta ocasión, el bikram se dividirá en :crossed_swords: Pair Programming :c
 
 <!-- - [ ] 5.- Crea la función **suma** que acepte como argumento **dos números** y devuelva **el resultado de su suma** -->
 
-- [ ] 6.- Crea la función **potenciacion** que acepte como argumento **dos números** y devuelva **el resultado de elevar el primero(a) al segundo(b) (a^b)**
+<!-- - [ ] 6.- Crea la función **potenciacion** que acepte como argumento **dos números** y devuelva **el resultado de elevar el primero(a) al segundo(b) (a^b)** -->
 
 - [ ] 7.- Crea la función **separarPalabras** que acepte como argumento **un string** y devuelva **un array de palabras 'hola mundo' => [hola, mundo]**
 
-- [ ] 8.- Crea la función **repetirString** que acepte como argumento **un string y un número** y devuelva **un string que sea el resultado de concatenar el primer string el número dado de veces**
+<!-- - [ ] 8.- Crea la función **repetirString** que acepte como argumento **un string y un número** y devuelva **un string que sea el resultado de concatenar el primer string el número dado de veces** -->
 
-- [ ] 9.- Crea la función **esPrimo** que acepte como argumento **un número** y devuelva ****true si es primo y false si no lo es****
+<!-- - [ ] 9.- Crea la función **esPrimo** que acepte como argumento **un número** y devuelva ****true si es primo y false si no lo es**** -->
 
 ### Mezclando arrays y funciones ###
 
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let arrayBidimensional = [[0, 1, 2], ['a', 'b', 'c']];
 
 //### Funciones ###
 
-
+//[ ] 5.
 function suma(num1, num2){
 
     return (num1 + num2)
@@ -19,15 +19,67 @@ function suma(num1, num2){
 }
 
 
+//[ ] 6.
 function potenciacion(N1, N2){
-    
-    
-    
+
+    let resultado = 1;
+    for (let i = 0; i < N2; i++) {
+        resultado = resultado * N1;
+    }
+    return resultado;
+     
 }
 
+// un bucle for para multiplicar la base N1 por sí misma N2 veces 
+// y almacenar el resultado en la variable resultado. 
+// La variable resultado se inicia en 1 para que la multiplicación funcione 
 
 
+//[ ] 7. Créez la fonction **separarPalabras** qui accepte **un string** comme 
+//argument et renvoie **un array de mots 'hola mundo' => [hola, mundo]**
+
+// function separarPalabras(string){
+//     // let palabra = [];
+//     // let palabra2 = "";
+//     // for (let i = 0; i < string.length; i++) {
+        
+//     //     if(string[i] === " "){
+//     //         palabra2 += ","
+//     //     }
+//     //     palabra2 += string[i]
+//     // }
+
+//     // return [palabra2]
 
 
+//     console.log (string.split(","))
+// }
 
+// separarPalabras("Hola Mundo")
+
+
+//[ ] 8.
+
+function repetirString(String, Numero){
+
+    let concatenar = "";
+    for (let i=0; i < Numero; i++){
+
+        concatenar += String
+    }
+
+    console.log(concatenar)
+    return concatenar
+}
+
+repetirString("ja")
+
+//[ ] 9.
+
+function esPrimo(n){
+    if (n<=1) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++)
+        if (n % i == 0) return false;
+    return true;
+}
 
